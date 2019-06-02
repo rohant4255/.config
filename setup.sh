ca614a6436984c84bcd620ac9def3cc52beda14b
Rohan Thakker <rohan.a.thakker@jpl.nasa.gov>
2019-06-02 04:06:33 -0700

    Added nitrogen wallpaper support.

--- setup.sh
+++ setup.sh
@@ -2,3 +2,5 @@
 # TODO: Add installation of light
 echo 'export PROMPT_COMMAND="pwd > /tmp/whereami"' >> ~/.bashrc
 sudo apt-get install xclip
+ln -s ~/.config/.gitignore_global .
+git config --global core.excludesfile ~/.gitignore_global
