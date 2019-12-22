bd4d26d0c189b1bf708e89deb417daead3e59e73
Rohan Thakker <rohan.a.thakker@jpl.nasa.gov>
2019-12-22 02:03:44 -0800

    Add tmux config.

--- setup.sh
+++ setup.sh
@@ -4,3 +4,5 @@ echo 'export PROMPT_COMMAND="pwd > /tmp/whereami"' >> ~/.bashrc
 sudo apt-get install xclip
 ln -s ~/.config/.gitignore_global .
 git config --global core.excludesfile ~/.gitignore_global
+[ -f ~/.tmux.conf ] && mv ~/.tmux.conf ~/.tmux.conf.old
+ln -s $PWD/.tmux.conf ~
