ef554b25fe67da4cdf0a364b892485e068cc41bc
Rohan Thakker <rohan.a.thakker@jpl.nasa.gov>
2020-02-29 02:40:54 -0800

    Add auto-start emacs on startup.

--- setup.sh
+++ setup.sh
@@ -6,3 +6,7 @@ ln -s ~/.config/.gitignore_global .
 git config --global core.excludesfile ~/.gitignore_global
 [ -f ~/.tmux.conf ] && mv ~/.tmux.conf ~/.tmux.conf.old
 ln -s $PWD/.tmux.conf ~
+# Setup autostart emacs
+sudo ln -s ~/.config/startemacs.sh /etc/init.d/
+sudo update-rc.d /etc/init.d/startemacs.sh defaults
+
