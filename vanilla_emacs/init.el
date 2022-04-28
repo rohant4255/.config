9b985133bf6baf266f91ce4d455afe5427a6725c
Rohan Thakker <rohant4255@gmail.com>
2022-04-28 01:20:31 -0700

    Add babel src templates.

--- vanilla_emacs/init.el
+++ vanilla_emacs/init.el
@@ -31,7 +31,7 @@
  '(helm-completion-style 'helm)
  '(helm-minibuffer-history-key "M-p")
  '(package-selected-packages
-   '(company lsp-ivy helm-lsp lsp-ui lsp-mode evil-collection org-bullets org-fancy-priorities helm undo-tree rainbow-delimiters evil doom-modeline which-key ivy use-package)))
+   '(org-tempo company lsp-ivy helm-lsp lsp-ui lsp-mode evil-collection org-bullets org-fancy-priorities helm undo-tree rainbow-delimiters evil doom-modeline which-key ivy use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
