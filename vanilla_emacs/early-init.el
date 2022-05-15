5a8253859e339400fa6c3fa4bea14465b098860d
Rohan Thakker <rohan.a.thakker@jpl.nasa.gov>
2022-05-14 20:53:49 -0700

    Update org keybindings and kill scratch buffer.

--- vanilla_emacs/early-init.el
+++ vanilla_emacs/early-init.el
@@ -1,3 +1,6 @@
 ;; Disable C-i to jump forward to restore TAB functionality in Org mode.
 (setq evil-want-C-i-jump nil)
 (setq vc-follow-symlinks t)
+(add-hook 'emacs-startup-hook (lambda ()
+                                (when (get-buffer "*scratch*")
+                                  (kill-buffer "*scratch*"))))
