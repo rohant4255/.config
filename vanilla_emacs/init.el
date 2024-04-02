ab913721ae539720e715668bbe0aec99e8c0f224
Rohan Thakker <roboticistrohan@gmail.com>
2024-04-01 22:50:00 -0700

    Org mode updates.

--- vanilla_emacs/init.el
+++ vanilla_emacs/init.el
@@ -29,8 +29,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   (quote
-    (evil-collection org-bullets org-fancy-priorities undo-tree rainbow-delimiters evil doom-modeline which-key ivy use-package))))
+   '(## org-bulletproof evil-collection org-bullets org-fancy-priorities undo-tree rainbow-delimiters evil doom-modeline which-key ivy use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
